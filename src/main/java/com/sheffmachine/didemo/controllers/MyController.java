ffb5b8bfd16342bc2b32a8b06c4e5f49c9521702
SHEFFmachine <sheff@sheffmachine.com>
2018-11-02 23:12:17 -0700

    Primary modifier

--- src/main/java/com/sheffmachine/didemo/controllers/MyController.java
+++ src/main/java/com/sheffmachine/didemo/controllers/MyController.java
@@ -1,11 +1,19 @@
 package com.sheffmachine.didemo.controllers;
 
+import com.sheffmachine.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private GreetingService greetingService;
+
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
         System.out.println("Hello!");
-        return "Food";
+        return greetingService.sayGreeting();
     }
 }
--- src/main/java/com/sheffmachine/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/sheffmachine/didemo/controllers/PropertyInjectedController.java
@@ -2,11 +2,13 @@ package com.sheffmachine.didemo.controllers;
 
 import com.sheffmachine.didemo.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingServiceImpl greetingService;
 
     public String sayHello() {
