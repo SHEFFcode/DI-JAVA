a91b6cb50c10b9d4223ac3e3b0e4667f21ac908e
SHEFFmachine <sheff@sheffmachine.com>
2018-11-02 23:18:37 -0700

    Spring profiles

--- src/main/java/com/sheffmachine/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/com/sheffmachine/didemo/services/PrimarySpanishGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile("es")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello from primary greeting service.";
+        return "Hola de Servicio de Soludo Primario.";
     }
 }
